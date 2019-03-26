e48d6cb1d5bea36422f67975b0eed4bfa5928a60
alulab14 <alulab14@inf.pucp.edu.pe>
2019-03-26 15:43:32 -0500

    Segundo commit

--- asd.c
+++ asd.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 int main(){
-	for(int i=0;i<100;i++){
+	int i;
+	for(i=0;i<100;i++){
 		if(i%3==0)printf("%d\n","fizz");
-		if(i%3==0)printf("%d\n","buzz");
+		if(i%5==0)printf("%d\n","buzz");
 		if(i%3==0 && i%5==0)printf("%d\n","fizz buzz");
 	}
 }
